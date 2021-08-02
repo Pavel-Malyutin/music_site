53a87201860fc89d7884c9a42c308d17cdb1d877
User <wer@qer.3>
2021-08-02 12:38:28 +0300

    fixed admin panel

--- albums/admin.py
+++ albums/admin.py
@@ -1,12 +1,77 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import *
 
-# Register your models here.
 
-admin.site.register(Band)
-admin.site.register(ArtistLabel)
-admin.site.register(Genre)
-admin.site.register(Album)
-admin.site.register(Images)
+@admin.register(Band)
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url')
+    list_display_links = ('name',)
+
+
+class ReviewInLine(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ('name', 'email',)
+
+
+class AlbumImagesInLine(admin.TabularInline):
+    model = Images
+    extra = 1
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="auto" height="60"')
+
+    get_image.short_description = 'Фото'
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'draft')
+    list_filter = ('category', 'year',)
+    search_fields = ('title', 'category__name')
+    inlines = [AlbumImagesInLine, ReviewInLine]
+    save_on_top = True
+    save_as = True
+    list_editable = ('draft',)
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'parent', 'album', 'id')
+    readonly_fields = ('name', 'email')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
+
+
+@admin.register(ArtistLabel)
+class ArtistLabelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'get_image')
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="auto" height="60"')
+
+    get_image.short_description = 'Фото'
+
+
+@admin.register(Images)
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'album', 'get_image')
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="auto" height="60"')
+
+    get_image.short_description = 'Фото'
+
+
 admin.site.register(RatingStar)
-admin.site.register(Reviews)
+
+admin.site.site_title = 'Панель администрирования'
+admin.site.site_header = 'Панель администрирования'
--- albums/apps.py
+++ albums/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AlbumsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'albums'
+    verbose_name = 'Музыкальный сайт'
